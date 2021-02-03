3a7bc22d44fadfdcb5a1c46562342450fe4e77be
bugrui <781404306@qq.com>
2021-02-03 17:36:33 +0800

    修复RShapeEditText无法输入

--- shape/src/main/java/com/bugrui/shape/RShapeEditText.kt
+++ shape/src/main/java/com/bugrui/shape/RShapeEditText.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.res.Resources
 import android.util.AttributeSet
 import android.util.TypedValue.applyDimension
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatEditText
 
 /**
@@ -17,9 +18,8 @@ import androidx.appcompat.widget.AppCompatEditText
  */
 class RShapeEditText @JvmOverloads constructor(
     context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : AppCompatEditText(context, attrs, defStyleAttr) {
+    attrs: AttributeSet? = null
+) : AppCompatEditText(context,attrs) {
 
     companion object {
         private const val DEF_VALUE_INT = -1
@@ -29,26 +29,26 @@ class RShapeEditText @JvmOverloads constructor(
 
 
     init {
-        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ShapeEditText)
-        val shapeTypeIndex = typedArray.getInt(R.styleable.ShapeEditText_shapeEtType, DEF_VALUE_INT_0)
-        val shapeBgColor = typedArray.getColor(R.styleable.ShapeEditText_shapeEtBgColor, DEF_VALUE_INT)
-        val shapeStrokeSize = typedArray.getInt(R.styleable.ShapeEditText_shapeEtStrokeSize, DEF_VALUE_INT)
-        val shapeStrokeColor = typedArray.getColor(R.styleable.ShapeEditText_shapeEtStrokeColor, DEF_VALUE_INT_0)
-        val shapeGradientTypeIndex = typedArray.getInt(R.styleable.ShapeEditText_shapeEtGradientType, DEF_VALUE_INT)
-        val shapeGradientOrientationIndex = typedArray.getInt(R.styleable.ShapeEditText_shapeEtGradientOrientation, DEF_VALUE_INT)
-        val shapeGradientColor1 = typedArray.getColor(R.styleable.ShapeEditText_shapeEtGradientColor1, DEF_VALUE_INT)
-        val shapeGradientColor2 = typedArray.getColor(R.styleable.ShapeEditText_shapeEtGradientColor2, DEF_VALUE_INT)
-        val shapeGradientColor3 = typedArray.getColor(R.styleable.ShapeEditText_shapeEtGradientColor3, DEF_VALUE_INT)
-        val shapeGradientColor4 = typedArray.getColor(R.styleable.ShapeEditText_shapeEtGradientColor4, DEF_VALUE_INT)
-        val shapeGradientColor5 = typedArray.getColor(R.styleable.ShapeEditText_shapeEtGradientColor5, DEF_VALUE_INT)
-        val shapeGradientColor6 = typedArray.getColor(R.styleable.ShapeEditText_shapeEtGradientColor6, DEF_VALUE_INT)
-        val shapeGradientColor7 = typedArray.getColor(R.styleable.ShapeEditText_shapeEtGradientColor7, DEF_VALUE_INT)
-        val shapeGradientColor8 = typedArray.getColor(R.styleable.ShapeEditText_shapeEtGradientColor8, DEF_VALUE_INT)
-        val shapeRadius = typedArray.getFloat(R.styleable.ShapeEditText_shapeEtRadius, DEF_VALUE_FLOAT)
-        val shapeLeftTopRadius = typedArray.getFloat(R.styleable.ShapeEditText_shapeEtLeftTopRadius, DEF_VALUE_FLOAT)
-        val shapeRightTopRadius = typedArray.getFloat(R.styleable.ShapeEditText_shapeEtRightTopRadius, DEF_VALUE_FLOAT)
-        val shapeLeftBottomRadius = typedArray.getFloat(R.styleable.ShapeEditText_shapeEtLeftBottomRadius, DEF_VALUE_FLOAT)
-        val shapeRightBottomRadius = typedArray.getFloat(R.styleable.ShapeEditText_shapeEtRightBottomRadius, DEF_VALUE_FLOAT)
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.RShapeEditText)
+        val shapeTypeIndex = typedArray.getInt(R.styleable.RShapeEditText_shapeEtType, DEF_VALUE_INT_0)
+        val shapeBgColor = typedArray.getColor(R.styleable.RShapeEditText_shapeEtBgColor, DEF_VALUE_INT)
+        val shapeStrokeSize = typedArray.getInt(R.styleable.RShapeEditText_shapeEtStrokeSize, DEF_VALUE_INT)
+        val shapeStrokeColor = typedArray.getColor(R.styleable.RShapeEditText_shapeEtStrokeColor, DEF_VALUE_INT_0)
+        val shapeGradientTypeIndex = typedArray.getInt(R.styleable.RShapeEditText_shapeEtGradientType, DEF_VALUE_INT)
+        val shapeGradientOrientationIndex = typedArray.getInt(R.styleable.RShapeEditText_shapeEtGradientOrientation, DEF_VALUE_INT)
+        val shapeGradientColor1 = typedArray.getColor(R.styleable.RShapeEditText_shapeEtGradientColor1, DEF_VALUE_INT)
+        val shapeGradientColor2 = typedArray.getColor(R.styleable.RShapeEditText_shapeEtGradientColor2, DEF_VALUE_INT)
+        val shapeGradientColor3 = typedArray.getColor(R.styleable.RShapeEditText_shapeEtGradientColor3, DEF_VALUE_INT)
+        val shapeGradientColor4 = typedArray.getColor(R.styleable.RShapeEditText_shapeEtGradientColor4, DEF_VALUE_INT)
+        val shapeGradientColor5 = typedArray.getColor(R.styleable.RShapeEditText_shapeEtGradientColor5, DEF_VALUE_INT)
+        val shapeGradientColor6 = typedArray.getColor(R.styleable.RShapeEditText_shapeEtGradientColor6, DEF_VALUE_INT)
+        val shapeGradientColor7 = typedArray.getColor(R.styleable.RShapeEditText_shapeEtGradientColor7, DEF_VALUE_INT)
+        val shapeGradientColor8 = typedArray.getColor(R.styleable.RShapeEditText_shapeEtGradientColor8, DEF_VALUE_INT)
+        val shapeRadius = typedArray.getFloat(R.styleable.RShapeEditText_shapeEtRadius, DEF_VALUE_FLOAT)
+        val shapeLeftTopRadius = typedArray.getFloat(R.styleable.RShapeEditText_shapeEtLeftTopRadius, DEF_VALUE_FLOAT)
+        val shapeRightTopRadius = typedArray.getFloat(R.styleable.RShapeEditText_shapeEtRightTopRadius, DEF_VALUE_FLOAT)
+        val shapeLeftBottomRadius = typedArray.getFloat(R.styleable.RShapeEditText_shapeEtLeftBottomRadius, DEF_VALUE_FLOAT)
+        val shapeRightBottomRadius = typedArray.getFloat(R.styleable.RShapeEditText_shapeEtRightBottomRadius, DEF_VALUE_FLOAT)
         typedArray.recycle()
 
         setShapeType(RShapeConstant.shapeTypeList[shapeTypeIndex])
